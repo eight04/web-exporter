0ef717135eb0f15174289c2d4195b47a13dc9c7c
eight04 <eight04@gmail.com>
2025-11-20 03:30:09 +0800

    Fix

--- src/lib/exporter.mjs
+++ src/lib/exporter.mjs
@@ -8,39 +8,41 @@ function init() {
   return {
     export: ({type, ...rest}) => {
       if (type === "media") {
-        return this.exportMedia(rest);
+        return exportMedia(rest);
       }
       if (type === "url") {
-        return this.exportUrl(rest);
-      }
-    },
-    exportUrl: ({input: urls}) => {
-      tasks.push(...urls.map(url => ({url})));
-    },
-    exportMedia: ({input: urls, default_ext, filename, ctx}) => {
-      ctx = {...ctx};
-      for (let i = 0; i < urls.length; i++) {
-        const url = new URL(urls[i]);
-        ctx.index = i;
-        const fileInfo = pathParse(url.pathname);
-        if (!ctx.filebase) {
-          ctx.filebase = fileInfo.base;
-        }
-        if (!ctx.filename) {
-          ctx.filename = fileInfo.name;
-        }
-        if (!ctx.ext) {
-          ctx.ext = fileInfo.ext || default_ext || ".jpg";
-        }
-        if (ctx.ext.includes(":")) {
-          // Handle cases like ".jpg:large"
-          ctx.ext = ctx.ext.split(":")[0];
-        }
-        const finalName = pyformat(filename, ctx);
-        tasks.push({url: url.href, filename: finalName});
+        return exportUrl(rest);
       }
     },
     getTasks: () => tasks.slice(),
     clearTasks: () => { tasks.length = 0; }
   };
+
+  function exportUrl({input: urls}) {
+    tasks.push(...urls.map(url => ({url})));
+  }
+
+  function exportMedia({input: urls, default_ext, filename, ctx}) {
+    ctx = {...ctx};
+    for (let i = 0; i < urls.length; i++) {
+      const url = new URL(urls[i]);
+      ctx.index = i;
+      const fileInfo = pathParse(url.pathname);
+      if (!ctx.filebase) {
+        ctx.filebase = fileInfo.base;
+      }
+      if (!ctx.filename) {
+        ctx.filename = fileInfo.name;
+      }
+      if (!ctx.ext) {
+        ctx.ext = fileInfo.ext || default_ext || ".jpg";
+      }
+      if (ctx.ext.includes(":")) {
+        // Handle cases like ".jpg:large"
+        ctx.ext = ctx.ext.split(":")[0];
+      }
+      const finalName = pyformat(filename, ctx);
+      tasks.push({url: url.href, filename: finalName});
+    }
+  }
 }
